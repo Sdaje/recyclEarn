e1b3033e60682d351526725b005743bf4da54cd5
Thomas Ben <46992388+ThomBen@users.noreply.github.com>
2022-08-30 11:54:48 +0200

    fix error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @lecture = Lecture.new(content: '<h1>Hello</h1>')
   end
 end
