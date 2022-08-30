4db088746f1a75ea32271f80f6fb1916e19fdff1
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-30 17:44:31 +0200

    start filters

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,6 +1,18 @@
 class InterestsController < ApplicationController
   def index
     @interests = Interest.all
+    # modele search avec paramètres de recherche (catégory), #validate_categories renvoit le tableau de catégorie recherché
+    # créer le modele search avec les catgeories commes variables
+    # revoyer ces catégories dans le formulaire comme bouton avec simple form @search et url: interests_path(<= renvoit vers interest mais est lié à @search)
+    # Dans le controleur interest, inité un where interest avec les catégories coché du @search via une méthode #validate_categories
+    if search_params
+      @search = Search.new(search_params)
+    else
+      # initialization d'un search vide pour le formulaire
+      @search = Search.new
+    end
+    @interests = @interests.where(category: @search.validate_categories)
+    # fin de recherche
     @markers = @interests.geocoded.map do |interest|
       case interest.category
       when "composter" then color = "#478978"
@@ -17,5 +29,10 @@ class InterestsController < ApplicationController
   end
 
   def search
+  end
+
+  private
+
+  def search_params
   end
 end
