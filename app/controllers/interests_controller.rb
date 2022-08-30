05b61a1b66fd02a377a8e660aa557d6f7676199b
sylvain Dajeans <sylvain.dajeans@gmail.com>
2022-08-30 13:45:51 +0200

    added map to index with custom markers

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,5 +1,16 @@
 class InterestsController < ApplicationController
   def index
+    @interests = Interest.all
+    @markers = @interests.geocoded.map do |interest|
+      case interest.category
+      when "composter" then image = "compost.png"
+      end
+      {
+        lat: interest.latitude,
+        lng: interest.longitude,
+        image_url: helpers.asset_url(image)
+      }
+    end
   end
 
   def search
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,7 +1,11 @@
 class Interest < ApplicationRecord
-  validates :address, presence: true
-  validates :website, presence: true
-  validates :category, presence: true
+  geocoded_by :address
+  after_validation :geocode
 
-  enum category: {}
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+
+  enum category: { composter: 1 }
 end
