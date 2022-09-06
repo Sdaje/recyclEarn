ca1f8cece115d97db04da082d2d4b96cc049ce28
Thomas Ben <46992388+ThomBen@users.noreply.github.com>
2022-09-06 11:48:11 +0200

    im done

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,137 +42,137 @@ lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 0/7"
-lecture = Lecture.new(title: "intro", content: "In this course, you will learn the different types of plastic you can recycle",learning_id: learninga.id)
+lecture = Lecture.new(title: "Apprendre", content: "<h5>Dans ce cours, vous allez apprendre à reconnaître les différents plastiques qui sont recyclables ainsi qu'apprendre ce qu'ils peuvent devenir une fois recyclés.</h5><br><p>Le plus important étant de reconnaître les différents labels, il existe 7 labels de plastique.</p><br><img src='https://blutopia.org/wp-content/uploads/2020/09/Label-Plastiques.jpg' alt='info-panel'>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 1/7"
-lecture = Lecture.new(title: "Polyethylene Terephthalate", content: "<h3>Polyethylene Terephthalate (PET)</h3>
+lecture = Lecture.new(title: "Polyethylene Terephthalate", content: "<h3>1. Polyethylene Terephthalate (PET)</h3>
 <p>C'est un plastique dûr souvent utilisé comme emballage à usage unique comme les emballages de nourriture ou les bouteilles en plastiques</p>
 <h4>Recyclage</h4>
 <p>C'est le plastique le plus couramment recyclé au monde.</p>
 <h4>Seconde vie</h4>
   <ul>
-    <li>Fibre pour tapis</li>
-    <li>Veste polaire, remplissage de couette, sacs fourre-tout</li>
-    <li>Emballage alimentaire et bouteilles en plastiques</li>
-    <li>Film alimentaire et bâches</li>
+    <li>- Fibre pour tapis</li>
+    <li>- Veste polaire, remplissage de couette, sacs fourre-tout</li>
+    <li>- Emballage alimentaire et bouteilles en plastiques</li>
+    <li>- Film alimentaire et bâches</li>
   </ul>
 <img src='https://www.nestle-waters.fr/sites/g/files/pydnoa621/files/asset-library/publishingimages/infographies/nsw-m-06-recherche-emballage-pet-v2_860px.jpg' alt='info-panel'>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 2/7"
-lecture = Lecture.new(title: "Polyethylene de haute densité", content: "<h3>Polyethylene de haute densité (PEH)</h3>
+lecture = Lecture.new(title: "Polyethylene de haute densité", content: "<h3>2. Polyethylene de haute densité (PEH)</h3>
 <p>C'est un plastique dûr moins transparent que le PET (slide précédent) souvent utilisé pour les bouteilles de produits d'entretien, de shampoings ou encore pour les emballages de yaourts.</p>
 <h4>Recyclage</h4>
 <p>Le HPDE est utilisé couramment car c'est un des plastiques les plus simples à recycler.</p>
-<h4>Seconde vie</h4>
+<h5>Seconde vie</h5>
   <ul>
-    <li>Bouteilles pour le non-alimentaire</li>
-    <li>Faux bois</li>
-    <li>Poubelles de recyclage</li>
-    <li>Carrelage</li>
-    <li>Seau, poubelle ou caisse</li>
-    <li>Peau de fleur</li>
+    <li>- Bouteilles pour le non-alimentaire</li>
+    <li>- Faux bois</li>
+    <li>- Poubelles de recyclage</li>
+    <li>- Carrelage</li>
+    <li>- Seau, poubelle ou caisse</li>
+    <li>- Peau de fleur</li>
   </ul>
 <img class='c-image-block__image' src='https://fourmize-sas.odoo.com/web/image/9015-c4b8782f/Post%20Facebook%20AOUT-3.png?access_token=a9ab852a-36ad-4426-9b8e-c1c0beb55322' alt=''>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 3/7"
-lecture = Lecture.new(title: "Polyvinyl Chloride", content: "<h3>Polyvinyl Chloride (PVC)</h3>
+lecture = Lecture.new(title: "Polyvinyl Chloride", content: "<h3>3. Polyvinyl Chloride (PVC)</h3>
 <p>Utilisé fréquemment pour les vinyls et les tuyaux, moins solide que le PET ou le HDPE. Le PVC est beaucoup utilisé du fait de son bas prix, de sa versatilité d'usage et sa durabilité.</p>
 <h4>Recyclage</h4>
 <p>Il est relativement difficile de recycler le PVC du fait de sa haute concentration en chlorine et d'additif variés. Il est spécifiquement séparé des autres plastiques en centre de tri pour permettre son recyclage.</p>
-<h4>Seconde vie</h4>
+<h5>Seconde vie</h5>
   <ul>
-    <li>Tuyaux</li>
-    <li>Présent dans la plupart des outils de jardinerie</li>
-    <li>Présent dans les tapis et les sols de maisons</li>
-    <li>Garde boue</li>
-    <li>Panneaux et cables électriques</li>
-    <li>Cônes de signalisation</li>
-    <li>Composants de voitures</li>
-    <li>Emballages, films, feuillets et draps </li>
+    <li>- Tuyaux</li>
+    <li>- Présent dans la plupart des outils de jardinerie</li>
+    <li>- Présent dans les tapis et les sols de maisons</li>
+    <li>- Garde boue</li>
+    <li>- Panneaux et cables électriques</li>
+    <li>- Cônes de signalisation</li>
+    <li>- Composants de voitures</li>
+    <li>- Emballages, films, feuillets et draps </li>
   </ul>
 <img class='c-image-block__image' src='https://volets-thiebaut.com/img/cms/Infographie-PVC-page-001_1.jpg' alt=''>", learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 4/7"
-lecture = Lecture.new(title: "Low Density Polyethylene (LDPE)", content: "  <h3>Low Density Polyethylene (LDPE)</h3>
+lecture = Lecture.new(title: "Low Density Polyethylene (LDPE)", content: "  <h3>4. Low Density Polyethylene (LDPE)</h3>
 <p>C'est un plastique souple souvent utilisés comme emballage de sachet de pain, sac de nourriture surgelé et sacs de course</p>
 <h4>Recyclage</h4>
 <p>Difficile à recycler mais toujours possible, nécessite d'être séparé des autres plastiques ainsi que trier par couleur en centre de tri.</p>
-<h4>Seconde vie</h4>
+<h5>Seconde vie</h5>
   <ul>
-    <li>Enveloppes</li>
-    <li>revêtemment de poubelles</li>
-    <li>Carrelage et panneaux de signalisation</li>
-    <li>Fournitures</li>
-    <li>Films plastiques et protections plastiques</li>
-    <li>Poubelles de bureau ou de compost</li>
-    <li>Faux bois</li>
+    <li>- Enveloppes</li>
+    <li>- revêtemment de poubelles</li>
+    <li>- Carrelage et panneaux de signalisation</li>
+    <li>- Fournitures</li>
+    <li>- Films plastiques et protections plastiques</li>
+    <li>- Poubelles de bureau ou de compost</li>
+    <li>- Faux bois</li>
   </ul>
 <img class='c-image-block__image' src='https://pbs.twimg.com/media/D4gcxFuWsAE7A4G?format=jpg&name=medium' alt=''>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 5/7"
-lecture = Lecture.new(title: "Polypropylene", content: "  <h3>Polypropylene (PP)</h3>
+lecture = Lecture.new(title: "Polypropylene", content: "  <h3>5. Polypropylene (PP)</h3>
 <p>Communément retrouvé dans les bouchons, les pailles et certains emballages de yaourts.</p>
 <h4>Recyclage</h4>
 <p>Un des plastiques les moins recyclés au monde (<1%). Son recyclage nécessite plus d'étapes et d'énergie que les autres plastiques.</p>
-<h3>Seconde vie</h3>
+<h5>Seconde vie</h5>
   <ul>
-    <li>Nombreux composants de voiture</li>
-    <li>Ballets, pinceaux, pic à glace, râteau de jardin.</li>
-    <li>Entonnoir à huile</li>
-    <li>Bacs de rangement</li>
-    <li>Plateaux, palette, bâches</li>
+    <li>- Nombreux composants de voiture</li>
+    <li>- Ballets, pinceaux, pic à glace, râteau de jardin.</li>
+    <li>- Entonnoir à huile</li>
+    <li>- Bacs de rangement</li>
+    <li>- Plateaux, palette, bâches</li>
   </ul>
 <img class='c-image-block__image' src='https://www.valorplast.com/wp-content/uploads/2015/03/PB_PEPPPS.png' alt=''>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 6/7"
-lecture = Lecture.new(title: "Polystyrene", content: "  <h3>Polystyrene (PS)</h3>
+lecture = Lecture.new(title: "Polystyrene", content: "  <h3>6. Polystyrene (PS)</h3>
 <p>Couramment trouvé dans les verres jetables et les emballages de nourriture à emporté, souvent appelé Styrofoam.</p>
 <h4>Recyclage</h4>
 <p>Principalement constitué d'air, il doit être compressé afin d'assurer son recyclage d'une manière effective.</p>
-<p>Seconde vie</p>
+<h5>Seconde vie</h5>
   <ul>
-    <li>Insulation thermique</li>
-    <li>Thermometre</li>
-    <li>Plaque d'immatriculation</li>
-    <li>Composant de caméra</li>
-    <li>Pour les moulages en plastiques</li>
-    <li>Etui protecteur</li>
-    <li>Interrupteur</li>
-    <li>Plateau de bureau</li>
-    <li>Carton pour les oeufs</li>
+    <li>- Insulation thermique</li>
+    <li>- Thermometre</li>
+    <li>- Plaque d'immatriculation</li>
+    <li>- Composant de caméra</li>
+    <li>- Pour les moulages en plastiques</li>
+    <li>- Etui protecteur</li>
+    <li>- Interrupteur</li>
+    <li>- Plateau de bureau</li>
+    <li>- Carton pour les oeufs</li>
   </ul>
 <img class='c-image-block__image' src='https://tricycle-mrcvs.ca/app/uploads/2021/10/Polystyrene_1348x800.jpg' alt=''>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "Adding a lesson in lecture 2 7/7"
-lecture = Lecture.new(title: "Les autres platiques", content: "  <h3>Les autres plastiques </h3>
+lecture = Lecture.new(title: "Les autres platiques", content: "  <h3>7. Les autres plastiques </h3>
   <p>D'autres types de plastiques peuvent être trouvés un peu partout</p>
   <h4>Recyclage</h4>
   <p>Très difficile à recycler par leurs variétés et la nécessité souvent de les séparer des autres plastiques. Il est donc très peu recyclé.</p>
-  <h4>Seconde vie</h4>
+  <h5>Seconde vie</h5>
     <ul>
-      <li>Bouteilles en plastiques</li>
-      <li>Faux bois</li>
+      <li>- Bouteilles en plastiques</li>
+      <li>- Faux bois</li>
     </ul>
   <img class='c-image-block__image' src='https://global-uploads.webflow.com/5cc9bbbe51a9add0b8662844/607adad541a0d7222e0fa8d1_Plastique%20environnement%20plane%CC%80te%20ze%CC%81ro%20de%CC%81chet%20diffe%CC%81rents%20types%20de%20plastique.jpg' alt=''>",learning_id: learninga.id)
 lecture.save!
 puts "lecture added to learning"
 
 puts "making question"
-quiz = Question.new(content: "Le PVC est un plastique", learning_id: learninga.id)
+quiz = Question.new(content: "Le PVC est un plastique qui est", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
@@ -195,7 +195,7 @@ option.save!
 puts "Options added succesfully, well done it's amazing"
 
 puts "making question 2"
-quiz = Question.new(content: "On différencie le PET et le PEH grâce à", learning_id: learninga.id)
+quiz = Question.new(content: "Comment peut-on différencier le PET et le PEH ?", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
@@ -218,7 +218,7 @@ option.save!
 puts "Options 2 added succesfully, well done it's amazing"
 
 puts "making question 3"
-quiz = Question.new(content: "Il existe", learning_id: learninga.id)
+quiz = Question.new(content: "De nombreux plastiques sont recyclables, et afin de faciliter cela, on les tries selon les :", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
@@ -241,7 +241,7 @@ option.save!
 puts "Options added succesfully, well done it's amazing"
 
 puts "making question 4"
-quiz = Question.new(content: "Pour les sachets de pains ou les sacs de nourriture surgelés, est utilisé principalement", learning_id: learninga.id)
+quiz = Question.new(content: "Quel est le plastique utilisé majoritairement pour les sachets de pains ou les sacs de nourriture surgelés ?", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
@@ -264,7 +264,7 @@ option.save!
 puts "Options added succesfully, well done it's amazing"
 
 puts "making question 5"
-quiz = Question.new(content: "Le plastique le plus recyclé au monde et donc le plus safe à acheté, c'est", learning_id: learninga.id)
+quiz = Question.new(content: "Quel est le plastique le plus recyclé au monde et donc le plus safe à acheté ?", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
@@ -287,7 +287,7 @@ option.save!
 puts "Options added succesfully, well done it's amazing and over"
 
 puts "making question"
-quiz = Question.new(content: "Le plastique le moins recyclé au monde, et donc le plus nocif, c'est", learning_id: learninga.id)
+quiz = Question.new(content: "Quel est le plastique le moins recyclé au monde, et donc le plus nocif parmis ceux recyclables ?", learning_id: learninga.id)
 quiz.save!
 puts "question saved"
 
